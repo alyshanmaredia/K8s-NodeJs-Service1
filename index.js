12500a122b02e0a51e9ea01d3e810bbe4b53ff66
Alishan Ali <Alishan.Ali@epmpartners.com.au>
2024-11-08 20:06:05 -0400

    adding service1 files

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+const app = express();
+const PORT = process.env.PORT || 6000;
+
+app.use(express.json());
+
+const STORAGE_PATH = path.join(__dirname, "files");
+
+if (!fs.existsSync(STORAGE_PATH)) {
+	fs.mkdirSync(STORAGE_PATH);
+}
+
+app.post("/store-file", (req, res) => {
+	const { file, data } = req.body;
+
+	if (!file || !data) {
+		return res.status(400).json({ file: null, error: "Invalid JSON input." });
+	}
+
+	const filePath = path.join(STORAGE_PATH, file);
+
+	fs.writeFile(filePath, data, (err) => {
+		if (err) {
+			return res
+				.status(500)
+				.json({ file, error: "Error while storing the file to the storage." });
+		}
+		return res.status(200).json({ file, message: "Success." });
+	});
+});
+
+app.post("/calculate", async (req, res) => {
+	const { file, product } = req.body;
+
+	console.log(req.body);
+
+	if (!file || !product) {
+		return res.status(400).json({ file: null, error: "Invalid JSON input." });
+	}
+
+	const filePath = path.join(STORAGE_PATH, file);
+
+	if (!fs.existsSync(filePath)) {
+		return res.status(404).json({ file, error: "File not found." });
+	}
+
+	try {
+		const response = await axios.post(`http://processor:7000/calculate-sum`, {
+			file,
+			product,
+		});
+
+		res.status(200).json(response.data);
+	} catch (error) {
+		console.error("Error calling Container 2:", error.message);
+		res
+			.status(500)
+			.json({ error: "Error while contacting Container 2 for calculation." });
+	}
+});
+
+app.listen(PORT, () => console.log(`Container 1 running on port ${PORT}`));
